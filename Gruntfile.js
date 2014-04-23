69eb4b040b269a815fe1563ed4aae51036d0bd6c
Emil Stenberg <esandin@gmail.com>
2014-04-23 09:33:29 +0200

    setting up tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,47 @@
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-sweet.js');
     grunt.initConfig({
+        sweetjs: {
+            options: {
+                sourceMap: false,
+                sourceRoot: '',
+                readableNames: true,
+                modules: [
+                    
+                ]
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'spec',
+                    src: '*.sjs',
+                    dest: '.tmp/spec',
+                    ext: '.js'
+                }]
+            }
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: [
+                    '.tmp/spec/**/*.js',
+                ]
+            }
+        },
+        test: {
+            files: [{
+                expand: true,
+                cwd: 'test/spec',
+                src: '{,*/}*.sjs',
+                dest: '.tmp/spec',
+                ext: '.js'
+            }]
+        },
         concat: {
             options: {
                 separator: '\n',
@@ -12,5 +52,12 @@ module.exports = function(grunt) {
                 dest: 'dist/macros.js',
             },
         },
+    });
+
+    grunt.registerTask('test', function() {
+        grunt.task.run([
+            'sweetjs',
+            'mochaTest'
+        ]);
     });
 };
---
+++ spec/as.sjs
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('as', function() {
+  describe('json', function() {
+    it('should stringify object', function() {
+      true.should.be(false);
+
+    });
+  })
+});
